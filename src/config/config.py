24b1051e487328afaa4e0bbb3af422a3a5deacbc
lepester <slavik55350@gmail.com>
2024-11-04 12:18:37 +0200

    updated BASE_URL

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 # config/config.py
 
-BASE_URL = "https://rozetka.com.ua"  # Target URL for performance testing
+BASE_URL = "https://exampleurl.com"  # Target URL for performance testing
 NUM_REQUESTS = 1000  # Total number of requests to be sent
 CONCURRENT_USERS = 100  # Number of users sending requests concurrently
 TIMEOUT = 5  # Timeout in seconds for each request
